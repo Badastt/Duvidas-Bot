2bf5544f1a27c225af6cd68863c8ffd45684978a
Jean <jeanrodriguesrocha@gmail.com>
2023-05-15 20:36:30 -0300

    config.js editado

--- data/config.js
+++ data/config.js
@@ -1,11 +1,11 @@
 
 var teste = false;
-module.exports.token = "[INSERT DISCORD USER TOKEN]";
+module.exports.token = "b324d7c64581961a20315a973f35d091da9ad0a3133b2a340a25ed949269779b";
 module.exports.prefixes = teste ? ["teste, "] : ["#"];
 
-module.exports.user = "[INSERT DATABASE USER HERE]";
-module.exports.host = "[INSERT DATABASE HOSTNAME HERE]";
-module.exports.database = "[INSERT DATABASE NAME HERE]";
-module.exports.password = "[INSERT DATABASE PASSWORD HERE]";
+module.exports.user = "postgres";
+module.exports.host = "containers-us-west-18.railway.app";
+module.exports.database = "postgresql://postgres:gJsWEkWxBYP4N9|P5Exg@container";
+module.exports.password = "gJsWEkWxBYP4N9ZP5Exg";
 
 // NEM TENTA OLHAR NOS COMMITS ANTIGOS, O BD ANTIGO TA MORTO E ENTERRADO, SAI DAQUI SEUS PILANTRA
